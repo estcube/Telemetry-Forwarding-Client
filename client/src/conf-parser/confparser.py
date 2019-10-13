1208ad2527dfa749b6f1db34e951844d0b251831
Märt Mäemees <martmaemees@gmail.com>
2019-10-13 11:45:18 +0300

    Small changes to conf parsing. #18.

--- client/src/conf-parser/confparser.py
+++ client/src/conf-parser/confparser.py
@@ -1,41 +1,43 @@
 import configparser
 
-
-config = configparser.RawConfigParser()
-config.read('../configuration.ini')
-
 TNC_Protocol_Type_types = ["KISS", "AGW"]
 TNC_Connection_Type_types = ["TCP/IP", "RS232"]
 
-constraints = {}
-sections = config.sections()
+class Configuration(object):
 
-for i in config["Constraints"]:
-    constraints[i] = config["Constraints"][i]
+    def __init__(self, path: str):
+        self.config = configparser.RawConfigParser()
+        self.configPath = path
+        self.config.read(path)
+        self.constraints = {}
+        self.sections = self.config.sections()
 
-# example: getConf("Mission Control", "relay-enabled")
-def getConf(section, element):
-    return config.get(section, element)
+        for i in self.config["Constraints"]:
+            self.constraints[i] = self.config["Constraints"][i]
 
-# example: setConf("Mission Control", "relay-enabled", False)
-def setConf(section, element, value):
-    
-    if(section not in sections):
-        raise ValueError("Section", sections, "does not exist")
-    
-    if(element not in constraints):
-        raise ValueError("Element", element, "is not in config")
+    # example: getConf("Mission Control", "relay-enabled")
+    def getConf(self, section, element):
+        return self.config.get(section, element)
 
-    if(element == "tnc-protocol-type" and value not in TNC_Protocol_Type_types):
-        raise ValueError("TNC-Protocol-Type only supports ", TNC_Protocol_Type_types)
-    
-    if(element == "tnc-connection-type" and value not in TNC_Connection_Type_types):
-        raise ValueError("TNC-Connection-Type only supports ", TNC_Connection_Type_types)
-    
-    if(type(value) is not eval(constraints.get(element))):
-        raise ValueError("Wrong type for element: ", element)
-    
-    config.set(section, element, value)
-    
-    with open('configuration.ini', 'w') as configfile:
-        config.write(configfile)
+    # example: setConf("Mission Control", "relay-enabled", False)
+    def setConf(self, section, element, value):
+
+        if(section not in self.sections):
+            raise ValueError("Section", section, "does not exist")
+
+        if(element not in self.constraints):
+            raise ValueError("Element", element, "is not in config")
+
+        if(element == "tnc-protocol-type" and value not in TNC_Protocol_Type_types):
+            raise ValueError("TNC-Protocol-Type only supports ", TNC_Protocol_Type_types)
+
+        if(element == "tnc-connection-type" and value not in TNC_Connection_Type_types):
+            raise ValueError("TNC-Connection-Type only supports ", TNC_Connection_Type_types)
+
+        if(type(value) is not eval(self.constraints.get(element))):
+            raise ValueError("Wrong type for element: ", element)
+
+        self.config.set(section, element, value)
+
+        with open(self.configPath, 'w') as configfile:
+            self.config.write(configfile)
