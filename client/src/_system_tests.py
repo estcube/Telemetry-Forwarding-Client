847d363d314a7179fc6e63dbeca3db804b344b7c
Märt Mäemees <martmaemees@gmail.com>
2019-11-11 03:31:17 +0200

    Fix systest.

--- client/src/_system_tests.py
+++ client/src/_system_tests.py
@@ -10,13 +10,13 @@ import os
 class SysTest(unittest.TestCase):
     testPath = os.path.join(os.path.dirname(__file__), "__test__")
     dbPath = os.path.join(testPath, "systest.db")
-    axPacket = b"\x8a\xa6j\x8a@@`\x8a\xa6j\x8a\x86@a\x03\xf0\x00\x00\x00\x01\x00q?GmN2dzpYLYwjafRIg30bY;BJ:K/JyOUu1tVqkch\\TN>dx~"
+    axPacket = b"\x8a\xa6j\x8a@@`\x8a\xa6j\x8a\x86@a\x03\xf0\x00\x00\x00\x01\x00q?GmN2dzpYLYwjafRIg30bY;BJ:K/JyOUu1tVqkch\\TN>dx"
 
     def test_checkDBEntry(self):
         conn = sqlite3.connect(self.dbPath)
         cur = conn.cursor()
         cur.execute("select data from ax_frame order by time desc limit 1;")
-        data = cur.fetchone()
+        data = cur.fetchone()[0]
 
         self.assertEqual(data, self.axPacket)
 
