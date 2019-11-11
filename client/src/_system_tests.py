a02ff49447ea750280fe6e4d37683e8704d1bcf6
Märt Mäemees <martmaemees@gmail.com>
2019-11-11 03:27:29 +0200

    Forgot first byte is stripped.

--- client/src/_system_tests.py
+++ client/src/_system_tests.py
@@ -10,7 +10,7 @@ import os
 class SysTest(unittest.TestCase):
     testPath = os.path.join(os.path.dirname(__file__), "__test__")
     dbPath = os.path.join(testPath, "systest.db")
-    axPacket = b"~\x8a\xa6j\x8a@@`\x8a\xa6j\x8a\x86@a\x03\xf0\x00\x00\x00\x01\x00q?GmN2dzpYLYwjafRIg30bY;BJ:K/JyOUu1tVqkch\\TN>dx~"
+    axPacket = b"\x8a\xa6j\x8a@@`\x8a\xa6j\x8a\x86@a\x03\xf0\x00\x00\x00\x01\x00q?GmN2dzpYLYwjafRIg30bY;BJ:K/JyOUu1tVqkch\\TN>dx~"
 
     def test_checkDBEntry(self):
         conn = sqlite3.connect(self.dbPath)
