bb757f63439db24dbe56e6130fccd17f9b168533
Taavet <taavet.simo@gmail.com>
2019-11-26 12:59:31 +0200

    #100 Added basic 404 page. ( No style)

---
+++ frontend/app/src/Components/PageNotFound.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+class PageNotFound extends React.Component {
+  render() {
+    return (
+      <div>
+        <div>404 Page not found</div>
+      </div>
+    );
+  }
+}
+
+export default PageNotFound;
