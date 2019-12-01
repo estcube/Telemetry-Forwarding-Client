1ec7f97d1648869056bda6272d6e2e05481eab5a
hendriksuvalov <bigbirdsrcute@gmail.com>
2019-12-01 11:00:29 +0200

    Made 404 page prettier

--- frontend/app/src/Components/PageNotFound.tsx
+++ frontend/app/src/Components/PageNotFound.tsx
@@ -1,15 +1,36 @@
 import * as React from 'react';
+import { createStyles, Typography, withStyles, WithStyles } from '@material-ui/core';
 
-class PageNotFound extends React.Component {
+const styles = () =>
+  createStyles({
+    divBox: {
+      display: 'block',
+      paddingTop: '5%'
+    },
+    logo: {
+      maxHeight: '100px',
+      maxWidth: '100px',
+      margin: 'auto',
+      display: 'block'
+    },
+    notFoundText: {
+      color: 'black',
+      textAlign: 'center'
+    }
+  });
+
+class PageNotFound extends React.Component<WithStyles<typeof styles>> {
   render() {
+    const { classes } = this.props;
     return (
-      <div>
-        <div>
-          <div>404 Page not found</div>
-        </div>
+      <div className={classes.divBox}>
+        <img src="././images/estcube.png" alt="logo" className={classes.logo} />
+        <Typography variant="h2" className={classes.notFoundText}>
+          Page not found
+        </Typography>
       </div>
     );
   }
 }
 
-export default PageNotFound;
+export default withStyles(styles)(PageNotFound);
