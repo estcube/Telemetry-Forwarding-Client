c1f49fb0c54d131d6f37f9d040f56210790e9fcf
Märt Mäemees <martmaemees@gmail.com>
2019-12-08 20:04:13 +0200

    Add requirements.txt file.

--- client/build.sh
+++ client/build.sh
@@ -10,7 +10,8 @@ cp -r "$BASEDIR/src" "$DISTDIR/src"
 rm -r "$DISTDIR"/src/_*
 cp -r "$BASEDIR/spec" "$DISTDIR/spec"
 cp "$BASEDIR/configuration.ini.sample" "$DISTDIR/configuration.ini"
-cp "$BASEDIR/README.md" "$DISTDIR/"
+cp "$BASEDIR/../README.md" "$DISTDIR/"
+cp "$BASEDIR/requirements.txt" "$DISTDIR/"
 
 # mkdir "$BASEDIR/dist/static"
 cp -r "$BASEDIR/../frontend/app/dist/" "$DISTDIR/static"
