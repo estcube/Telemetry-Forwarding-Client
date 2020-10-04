f2658871108aa5eea2e5fbeaf32075ac74457cee
Risto-R <rahulan.risto@gmail.com>
2020-10-04 22:32:14 +0300

    Added newline

--- client/src/file_logger.py
+++ client/src/file_logger.py
@@ -1,6 +1,7 @@
 import logging
 from ax_listener import AXFrame
 
+
 class FileLogger():
 
     def __init__(self, logfile):
@@ -8,5 +9,6 @@ class FileLogger():
         self.file_logger = logging.FileHandler(logfile)
 
     """ Log received package to a .log file"""
+
     def log_ax_frame(self, frame: AXFrame):
         self.file_logger.handle(self._logger.makeRecord(None, logging.DEBUG, None, None, str(frame), None, None))
