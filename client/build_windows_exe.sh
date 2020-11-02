20a1d3d4360f1b2cf5d5446e122169546d85b704
unknown <teetsaar7@gmail.com>
2020-11-02 15:30:34 +0200

    Deleted old files and improved flow

--- client/build_windows_exe.sh
+++ client/build_windows_exe.sh
@@ -2,15 +2,11 @@
 
 BASEDIR=$(dirname "$BASH_SOURCE")
 DISTDIR="$BASEDIR/../dist"
-
-rm -rf "$DISTDIR"
 mkdir "$DISTDIR"
-
-cp -r "$BASEDIR/src" "$DISTDIR/src"
-rm -r "$DISTDIR"/src/_*
 cp -r "$BASEDIR/spec" "$DISTDIR/spec"
 cp "$BASEDIR/configuration.ini.sample" "$DISTDIR/configuration.ini"
 cp "$BASEDIR/../README.md" "$DISTDIR/"
 
-# mkdir "$BASEDIR/dist/static"
+cp "$BASEDIR/dist/main.exe" "$DISTDIR/"
+
 cp -r "$BASEDIR/../frontend/app/dist/" "$DISTDIR/static"
