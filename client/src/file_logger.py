1f993512d779a0e2d00a246938bb6ef8417eeb33
Risto-R <rahulan.risto@gmail.com>
2020-12-01 12:48:50 +0200

    Use log path given in config

--- client/src/file_logger.py
+++ client/src/file_logger.py
@@ -5,6 +5,7 @@ from conf import Configuration
 import requests
 import os
 
+
 class FileLogger():
     log = logging.getLogger(__name__)
 
@@ -16,11 +17,12 @@ class FileLogger():
             if tle_req.status_code == 200:
                 mission_name = tle_req.text.split("\n")[1][2:7]
         except:
-            self.log.warning("Connection to tle endpoint failed")
+            self.log.warning(
+                "Connection to tle endpoint failed. Using default packet log file name ({})".format(mission_name))
 
         if not os.path.isdir(path):
             os.mkdir(path)
-        logfile = "{}{}_packets.log".format(path, mission_name)
+        logfile = "{}/{}_packets.log".format(path, mission_name)
         self.file_logger = logging.FileHandler(logfile)
 
     def log_ax_frame(self, frame: AXFrame):
